85e100be35d795d4053473e60bbb3c044d096296
nikoarap <nikoarap@gmail.com>
2023-10-27 15:12:57 +0300

    updated storage class and function

--- app/src/main/java/com/nikoarap/gametime/storage/SportsStorage.kt
+++ app/src/main/java/com/nikoarap/gametime/storage/SportsStorage.kt
@@ -1,4 +1,15 @@
 package com.nikoarap.gametime.storage
 
-class SportsStorage {
+import com.nikoarap.gametime.models.SportModel
+import com.nikoarap.gametime.utils.RealmUtils
+
+open class SportsStorage {
+
+    companion object {
+
+        fun insert(sportModel: SportModel) {
+            RealmUtils.executeTransaction { realm -> realm.insertOrUpdate(sportModel) }
+        }
+    }
+
 }
