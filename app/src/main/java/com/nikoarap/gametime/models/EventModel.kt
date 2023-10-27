415f441610472fd4b944e11af9218bd89813640d
nikoarap <nikoarap@gmail.com>
2023-10-27 12:01:00 +0300

    made models extend realm model

--- app/src/main/java/com/nikoarap/gametime/models/EventModel.kt
+++ app/src/main/java/com/nikoarap/gametime/models/EventModel.kt
@@ -1,10 +1,14 @@
 package com.nikoarap.gametime.models
 
 import com.nikoarap.gametime.utils.Constants.Companion.EMPTY
+import io.realm.RealmModel
+import io.realm.annotations.PrimaryKey
 
-data class EventModel(
-    var id: String = EMPTY,
-    var sportId: String = EMPTY,
-    var name: String = EMPTY,
-    var startTime: Int = 0,
-)
+open class EventModel: RealmModel {
+    @PrimaryKey
+    var id: String = EMPTY
+    var sportId: String = EMPTY
+    var name: String = EMPTY
+    var startTime: Int = 0
+}
+
--- app/src/main/java/com/nikoarap/gametime/models/SportModel.kt
+++ app/src/main/java/com/nikoarap/gametime/models/SportModel.kt
@@ -1,9 +1,15 @@
 package com.nikoarap.gametime.models
 
 import com.nikoarap.gametime.utils.Constants.Companion.EMPTY
+import io.realm.RealmModel
+import io.realm.annotations.PrimaryKey
 
-data class SportModel(
-    var id: String = EMPTY,
-    var name: String = EMPTY,
+open class SportModel: RealmModel {
+    @PrimaryKey
+    var id: String = EMPTY
+    var name: String = EMPTY
     var activeEvents: ArrayList<EventModel> = arrayListOf()
-)
+}
+
+
+
