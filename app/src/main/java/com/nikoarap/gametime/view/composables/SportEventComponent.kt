e69cfe5368945cb9aff1bc3d2a7d21c3ba95eea2
nikoarap <nikoarap@gmail.com>
2023-10-28 16:02:53 +0300

    updated togglable favourite icon

--- app/src/main/java/com/nikoarap/gametime/view/composables/SportEventComponent.kt
+++ app/src/main/java/com/nikoarap/gametime/view/composables/SportEventComponent.kt
@@ -8,18 +8,30 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.sharp.Star
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.nikoarap.gametime.R
 import com.nikoarap.gametime.models.EventModel
 import com.nikoarap.gametime.utils.Constants
 import com.nikoarap.gametime.utils.Constants.Companion.VS_VALUE
 import com.nikoarap.gametime.view.themes.dp_16
 import com.nikoarap.gametime.view.themes.dp_24
+import com.nikoarap.gametime.view.themes.dp_4
 import com.nikoarap.gametime.view.themes.dp_8
 import com.nikoarap.gametime.view.themes.onSurface
 import com.nikoarap.gametime.view.themes.secondary
@@ -30,6 +42,8 @@ import com.nikoarap.gametime.view.themes.tertiary
 fun LoadSportEvent(
     event: EventModel
 ) {
+    var isEventFavourite by remember { mutableStateOf(event.isFavourite) }
+
     Column(
         modifier = Modifier
             .background(color = surface)
@@ -39,8 +53,15 @@ fun LoadSportEvent(
     ) {
         TextWithBorder(text = event.startTime.toString())
         Icon(
-            modifier = Modifier.size(dp_24).clickable {  },
-            painter = painterResource(id = R.drawable.ic_star_hollow),
+            painter = if (isEventFavourite) rememberVectorPainter(Icons.Filled.Star) else painterResource(id = R.drawable.ic_star_hollow),
+            modifier = Modifier
+                .padding(dp_4)
+                .size(dp_24)
+                .clickable {
+                    isEventFavourite = !isEventFavourite
+                    //realm logic here
+                },
+
             contentDescription = Constants.ICON,
             tint = onSurface
         )
