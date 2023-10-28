352f55b4d50bc2e93964faa52c2538a74ce827c3
nikoarap <nikoarap@gmail.com>
2023-10-28 11:51:15 +0300

    updated material dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.compose.material3:material3")
 
     //realm lib
