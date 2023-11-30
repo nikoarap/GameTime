2632b1f7dced45d5c28b2ae4d046ffefec4528fe
nikoarap <nikoarap@gmail.com>
2023-11-30 17:22:18 +0200

    restructured packages

--- app/src/androidTest/java/com/nikoarap/gametime/feature_sports/presentation/common/CountdownTimerTest.kt
+++ app/src/androidTest/java/com/nikoarap/gametime/feature_sports/presentation/common/CountdownTimerTest.kt
@@ -1,41 +1,40 @@
-package com.nikoarap.gametime
+package com.nikoarap.gametime.feature_sports.presentation.common
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import com.nikoarap.gametime.feature_sports.presentation.common.CountdownTimer
 import org.junit.Rule
 import org.junit.Test
 
-class TimerTest {
+class CountdownTimerTest {
 
     @get:Rule
     val testRule = createComposeRule()
 
     @Test
-    fun testCountdownValueMinute() {
-        val timeUntilEventStartInMs = 60000L // 1 minute
+    fun countDownMinuteExpectedText_assertExists() {
+        val minuteInMs = 60000L
         testRule.setContent {
-            CountdownTimer(timeUntilEventStartInMs)
+            CountdownTimer(minuteInMs)
         }
         val expectedText = "1m, 00s"
         testRule.onNodeWithText(expectedText).assertExists()
     }
 
     @Test
-    fun testCountdownValueDaysMonthsMinutesSeconds() {
-        val timeUntilEventStartInMs = 16965052000 //196 days, 8 hours, 30 minutes and 52 seconds
+    fun countdownDaysMonthsMinutesSecondsText_assertExists() {
+        val timeInMs = 16965052000 //196 days, 8 hours, 30 minutes and 52 seconds
         testRule.setContent {
-            CountdownTimer(timeUntilEventStartInMs)
+            CountdownTimer(timeInMs)
         }
         val expectedText = "196d, 8h, 30m, 52s"
         testRule.onNodeWithText(expectedText).assertExists()
     }
 
     @Test
-    fun testCountdownValueZero() {
-        val timeUntilEventStartInMs = 0L
+    fun countdownZeroTimeText_assertExists() {
+        val zeroTimeInMs = 0L
         testRule.setContent {
-            CountdownTimer(timeUntilEventStartInMs)
+            CountdownTimer(zeroTimeInMs)
         }
         val expectedText = "EVENT STARTED"
         testRule.onNodeWithText(expectedText).assertExists()
